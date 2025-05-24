6361f9bef7cc229eb6f7cee9df2c50090d103f21
Pranay Kumar Singh <pranaysingh.nk@gmail.com>
2025-05-24 23:15:58 +0530

    Update example.java
    Added new Print statements

--- example.java
+++ example.java
@@ -1,5 +1,5 @@
-class Factorial{
-    public static void main(String[] a){
+#class Factorial{
+#    public static void main(String[] a){
         System.out.println(10);
     }
 }
@@ -27,5 +27,7 @@ class Exemplo2{
             System.out.println(100);
         }
         return a ;
+        int b = System.out.println();
+        int c = System.out.println();
     }
 }
